e7159c8894ac10ab9fd09e316605b88459ec1284
Jair Anguiano <jairanpo@gmail.com>
2020-04-07 12:01:55 -0500

    Removed videos

--- src/components/background_video.js
+++ src/components/background_video.js
@@ -1,4 +1,3 @@
-const mp4 = require('../static/videos/3840x2160.mp4');
 const webm = require('../static/videos/3840x2160.webm');
 const image = require('../static/images/background.png');
 
@@ -6,7 +5,7 @@ var html = `
     <div>
         <video playsinline autoplay muted loop poster=${image} id="bgvid">
         <source src=${webm} type="video/webm">
-        <source src=${webm} type="video/mp4">
+        
         </video>
     </div>
 `
